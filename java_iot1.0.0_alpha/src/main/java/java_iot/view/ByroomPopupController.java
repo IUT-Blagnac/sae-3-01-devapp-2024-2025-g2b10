7e379aa869b3abeb45d85c00fc6f86a26376f969
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-11-29 10:40:45 +0100

    Popups
    Squelette des popups pour Byroom et Bypanel

---
+++ java_iot1.0.0_alpha/src/main/java/java_iot/view/ByroomPopupController.java
@@ -0,0 +1,20 @@
+package java_iot.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class ByroomPopupController {
+
+    private Navbar navigationBar;
+
+	@FXML
+	private Button submit;
+
+    @FXML
+    private ChoiceBox choice;
+    
+}
